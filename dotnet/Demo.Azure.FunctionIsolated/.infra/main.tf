aea5259352842d2b3b94b8e926cc870571cab9d3
Alexander Pykavy <aleksandrpykavyj@gmail.com>
2024-03-08 16:49:49 +0100

    Update AzureFunction isolated project to work with both SA key & managed identity

--- dotnet/Demo.Azure.FunctionIsolated/.infra/main.tf
+++ dotnet/Demo.Azure.FunctionIsolated/.infra/main.tf
@@ -1,6 +1,13 @@
+variable "use_managed_identity" {
+  default = true
+}
+
 provider "azurerm" {
   features {}
+  storage_use_azuread = true
 }
+
+data "azuread_client_config" "current" {}
 
 resource "random_id" "main" {
   byte_length = 8
@@ -16,16 +23,26 @@ resource "azurerm_resource_group" "main" {
 }
 
 resource "azurerm_storage_account" "main" {
-  name                     = "${lower(random_id.main.hex)}sa"
-  resource_group_name      = azurerm_resource_group.main.name
-  location                 = azurerm_resource_group.main.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+  name                            = "${lower(random_id.main.hex)}sa"
+  resource_group_name             = azurerm_resource_group.main.name
+  location                        = azurerm_resource_group.main.location
+  account_tier                    = "Standard"
+  account_replication_type        = "LRS"
+  default_to_oauth_authentication = var.use_managed_identity ? true : false
+  shared_access_key_enabled       = var.use_managed_identity ? false : true
 
   network_rules {
     default_action = "Deny"
     ip_rules       = [chomp(data.http.myip.response_body)]
   }
+}
+
+resource "azurerm_role_assignment" "current_to_sa" {
+  for_each = var.use_managed_identity ? toset(["Storage Account Contributor", "Storage Blob Data Owner", "Storage Queue Data Contributor"]) : []
+
+  scope                = azurerm_storage_account.main.id
+  role_definition_name = each.key
+  principal_id         = data.azuread_client_config.current.object_id
 }
 
 resource "azurerm_storage_queue" "main" {
@@ -34,5 +51,13 @@ resource "azurerm_storage_queue" "main" {
 }
 
 output "AzureWebJobsStorage" {
-  value = nonsensitive(azurerm_storage_account.main.primary_connection_string)
+  value = var.use_managed_identity ? null : (azurerm_storage_account.main.primary_connection_string)
+}
+
+output "AzureWebJobsStorage__blobServiceUri" {
+  value = var.use_managed_identity ? azurerm_storage_account.main.primary_blob_endpoint : null
+}
+
+output "AzureWebJobsStorage__queueServiceUri" {
+  value = var.use_managed_identity ? azurerm_storage_account.main.primary_queue_endpoint : null
 }
