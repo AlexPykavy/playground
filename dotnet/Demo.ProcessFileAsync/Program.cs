e954186a39822b425c66355da7f3293b15265616
Alexander Pykavy <aleksandrpykavyj@gmail.com>
2025-08-22 13:06:52 +0200

    Add an example of async file reading

---
+++ dotnet/Demo.ProcessFileAsync/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+// ConcurrentQueue<string?> q = new ConcurrentQueue<string?>();
+Queue<string?> q = new Queue<string?>();
+
+using (StreamReader sr = new StreamReader("big_file.txt"))
+{
+    while (true)
+    {
+        Task<string?> readTask = sr.ReadLineAsync();
+
+        while (q.TryDequeue(out string? message))
+        {
+            System.Console.WriteLine(message);
+        }
+
+        string? line = await readTask;
+        if (line == null)
+        {
+            break;
+        }
+
+        q.Enqueue(line);
+    }
+}
