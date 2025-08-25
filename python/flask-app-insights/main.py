1d3629437506ca35ab197e311f75e72b9c192148
Alexander Pykavy <aleksandrpykavyj@gmail.com>
2025-08-25 14:34:28 +0200

    Add a sample on how to integrate Flask app with Azure App Insights

---
+++ python/flask-app-insights/main.py
@@ -0,0 +1,54 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+"""
+An example to show an application instrumented with the OpenTelemetry flask instrumentation.
+Calls made with the flask library will be automatically tracked and telemetry is exported to 
+application insights with the AzureMonitorTraceExporter.
+See more info on the flask instrumentation here:
+https://github.com/open-telemetry/opentelemetry-python-contrib/tree/main/instrumentation/opentelemetry-instrumentation-flask
+"""
+# mypy: disable-error-code="attr-defined"
+import os
+import flask
+import time
+
+from opentelemetry import trace
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+from azure.monitor.opentelemetry.exporter import AzureMonitorTraceExporter
+
+# This method instruments all of FastAPI.
+# You can also use FlaskInstrumentor().instrument_app(app) to instrument a specific app after it is created.
+FlaskInstrumentor().instrument()
+app = flask.Flask(__name__)
+
+trace.set_tracer_provider(TracerProvider())
+tracer = trace.get_tracer(__name__)
+span_processor = BatchSpanProcessor(
+    AzureMonitorTraceExporter.from_connection_string(
+        os.environ["APPLICATIONINSIGHTS_CONNECTION_STRING"]
+    )
+)
+trace.get_tracer_provider().add_span_processor(span_processor)
+
+@app.route("/")
+def test():
+    time.sleep(0.5)
+
+    current_span = trace.get_current_span()
+
+    current_span.set_attribute("operation.value", 1)
+    current_span.set_attribute("operation.name", "Saying hello!")
+    current_span.set_attribute("operation.other-stuff", [1, 2, 3])
+
+    return "Test flask request"
+
+@app.route("/error")
+def error():
+    raise TypeError("ERROR!")
+
+
+if __name__ == "__main__":
+    app.run(host="localhost", port=8081, threaded=True)
